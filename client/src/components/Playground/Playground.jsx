6f46b5cebc2b287bf2a1d1ab91e6e5c1014de994
Victor Ngo <psychosete@gmail.com>
2017-07-19 01:14:07 -0700

    (feat) Implement socket.io pairing between clients

--- client/src/components/Playground/Playground.jsx
+++ client/src/components/Playground/Playground.jsx
@@ -7,16 +7,15 @@ class Playground extends Component {
   constructor (props) {
     super(props);
 
-    this.handleRunClick = props.handleRunClick.bind(this);
-    this.handleClearClick = props.handleClearClick.bind(this);
+    this.handleRunClick = props.handleRunClick;
+    this.handleClearClick = props.handleClearClick;
   }
 
   componentDidMount () {
-
-    console.log('Playground mounted..f.');  
+    console.log('Playground mounted...');  
     console.log('Code mirror loading...');
     var textArea = document.getElementById('code');
-    var defaultString = 'function myScript() {\n\treturn 100;\n}\nconsole.log(myScript());';
+    var defaultString = this.props.editorCode;
     var options = {
       // indentUnit: 2,
       lineNumbers: true,
@@ -28,8 +27,22 @@ class Playground extends Component {
     textArea.value = defaultString;
     var codeMirror = CodeMirror.fromTextArea(textArea, options);
     codeMirror.setSize('100%', '100%');
-    codeMirror.on('change', function(cm, change) {
+    codeMirror.on('keyup', (cm, change) => {
+      console.log('Change', change);
+      this.props.socket.emit('changed_code', codeMirror.getValue());
       cm.save(); // saves editor's value to textarea value
+    });
+    console.log('Mirror:', document.getElementsByClassName('CodeMirror'));
+    document.getElementsByClassName('CodeMirror')[0].addEventListener('keypress', _ => {
+      console.log('pressed a key on codemirror!');
+      
+    });
+    this.props.socket.on('changed_code', (code) => {
+      codeMirror.setValue(code);
+      // codeMirror.on('change', (cm, change) => {
+      //   this.props.socket.emit('changed_code', code);
+      //   cm.save(); // saves editor's value to textarea value
+      // });
     });
   }
 
@@ -38,7 +51,13 @@ class Playground extends Component {
     return (
       <div className="playground">
         <div className="buttons">
-          <button onClick={this.handleRunClick} className="run">Run</button>  
+          <button onClick={ _ => {
+            this.handleRunClick();
+            }} 
+            className="run"
+          >
+          Run
+          </button>  
           <button onClick={this.handleClearClick} className="run">Clear</button>  
         </div>
         <textarea id="code"></textarea>
