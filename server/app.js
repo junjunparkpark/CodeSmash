4ff6f15599d79bcf37aa6848a0dd92758abd68d4
Victor Ngo <psychosete@gmail.com>
2017-07-21 16:24:12 -0700

    (test) Uncomment rest of code to experiment

--- server/app.js
+++ server/app.js
@@ -7,49 +7,49 @@ const routes = require('./routes');
 const app = express();
 
 // SOCKET IO TENTATIVE CODE
-// const http = require('http').createServer(app);
-// const io = require('socket.io')(http);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-// app.use(middleware.morgan('dev'));
-// app.use(middleware.cookieParser());
-// console.log('Body parser adding...');
-// app.use(middleware.bodyParser.urlencoded({extended: false}));
-// console.log('Body parser JSON...');
-// app.use(middleware.bodyParser.json());
+app.use(middleware.morgan('dev'));
+app.use(middleware.cookieParser());
+console.log('Body parser adding...');
+app.use(middleware.bodyParser.urlencoded({extended: false}));
+console.log('Body parser JSON...');
+app.use(middleware.bodyParser.json());
 // console.log('Adding views...');
 app.set('views', path.join(__dirname, 'views'));
 console.log('Loading view engine...');
 app.set('view engine', 'ejs');
 
-// app.use(middleware.auth.session);
-// app.use(middleware.passport.initialize());
-// app.use(middleware.passport.session());
-// app.use(middleware.flash());
+app.use(middleware.auth.session);
+app.use(middleware.passport.initialize());
+app.use(middleware.passport.session());
+app.use(middleware.flash());
 
 app.use(express.static(path.join(__dirname, '../public')));
 
 console.log('Using router to deal with /*  BUT NOT /api endpoint ...');
-// app.use('/', routes.auth);
-app.get('/', (req, res) => { res.render('index.ejs'); });
-// app.use('/api', routes.api);
-// app.use('/api/profiles', routes.profiles);
+app.use('/', routes.auth);
+// app.get('/', (req, res) => { res.render('index.ejs'); });
+app.use('/api', routes.api);
+app.use('/api/profiles', routes.profiles);
 
 // SOCKET IO TENTATIVE CODE
 // http.listen(process.env.port || 3000, function(){
 //   console.log('listening on *:3000');
 // });
 
-// io.on('connection', function (socket) {
-//   console.log('a user connected!');
-//   socket.on('changed_code', function (code) {
-//     console.log('user changed code:', code);
-//     socket.broadcast.emit('changed_code', code);
-//   });
+io.on('connection', function (socket) {
+  console.log('a user connected!');
+  socket.on('changed_code', function (code) {
+    console.log('user changed code:', code);
+    socket.broadcast.emit('changed_code', code);
+  });
 
-//   socket.on('executed_code', function (code) {
-//     console.log('Executed code:', code);
-//     socket.broadcast.emit('executed_code', code);
-//   });
+  socket.on('executed_code', function (code) {
+    console.log('Executed code:', code);
+    socket.broadcast.emit('executed_code', code);
+  });
 
   socket.on('cleared_terminal', function (code) {
     console.log('Broadcasting cleared code event!', code);
@@ -60,5 +60,5 @@ app.get('/', (req, res) => { res.render('index.ejs'); });
     console.log('user disconnected...');
   });
 
-// });
-module.exports = app;
+});
+module.exports = http;
