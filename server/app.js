3cd72267a433fe530d2b14782fead988ec529e0e
Victor Ngo <psychosete@gmail.com>
2017-07-21 12:20:59 -0700

    (test) Change server code to get Heroku deploy

--- server/app.js
+++ server/app.js
@@ -10,17 +10,17 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.use(middleware.morgan('dev'));
-app.use(middleware.cookieParser());
+// app.use(middleware.morgan('dev'));
+// app.use(middleware.cookieParser());
 app.use(middleware.bodyParser.urlencoded({extended: false}));
 app.use(middleware.bodyParser.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(middleware.auth.session);
-app.use(middleware.passport.initialize());
-app.use(middleware.passport.session());
-app.use(middleware.flash());
+// app.use(middleware.auth.session);
+// app.use(middleware.passport.initialize());
+// app.use(middleware.passport.session());
+// app.use(middleware.flash());
 
 app.use(express.static(path.join(__dirname, '../public')));
 
