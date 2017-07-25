969725c395498b7f4ed9678019d53ccf0bf3ccde
Jun Park <jun.park@live.com>
2017-07-24 21:40:32 -0700

    Add database schema for MongoDB

--- db/index.js
+++ db/index.js
@@ -1,7 +1,59 @@
-const knex = require('knex')(require('../knexfile'));
-const db = require('bookshelf')(knex);
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
 
-db.plugin('registry');
+const db = mongoose.connection;
 
-module.exports = db;
+db.on('error', function() {
+  console.log('mongoose connection error');
+});
 
+db.once('open', function() {
+  console.log('mongoose connected successfully');
+});
+
+var userSchema = mongoose.Schema(
+  {
+    'id': String,
+    'email': String,
+    'hash': String,
+    'image_url': String,
+    'url': String,
+    'first_name': String,
+    'last_name': String,
+    'company': String,
+    'title': String,
+    'location': {
+      'city': String,
+      'state': String
+    },
+    'interviews': [
+      {
+        'date': { type: Date, default: Date.now },
+        'start_time': String,
+        'room_url': String,
+        'interviewee': String,
+        'code': [
+          {
+            'time': String,
+            'code_snippet': String
+          }
+        ]
+      }
+    ]
+  }
+);
+
+var User = mongoose.model('User', userSchema);
+
+var selectAll = function(callback) {
+  User.find({}, function(err, items) {
+    if(err) {
+      callback(err, null);
+    } else {
+      callback(null, items);
+    }
+  });
+};
+
+module.exports = User;
+module.exports.selectAll = selectAll;
