ee47247b0f653bc260580e1453da628cd230da50
Jun Park <jun.park@live.com>
2017-07-24 21:51:32 -0700

    Implement API route for GET and POST to Users table in Mongo

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,7 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const Users = ('../index.js');
 
 router.route('/')
   .get((req, res) => {
@@ -9,6 +10,15 @@ router.route('/')
   .post((req, res) => {
     console.log('in the correct route');
     res.status(201).send({ data: 'Posted!' });
+  });
+
+router.route('/api/:users')
+  .get((req, res) => {
+    res.status(200).send(Users.selectAll());
+  })
+  .post((req, res) => {
+    console.log('POSTing to Users');
+    res.status(201).send({ data: 'POST to Users' });
   });
 
 module.exports = router;
